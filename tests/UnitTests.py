d9de2345075d070433aa06d1533d2195976fcded
Mateus Fonseca <71104330+mateusf-prog@users.noreply.github.com>
2025-09-18 20:23:40 -0300

    create more tests

--- tests/UnitTests.py
+++ tests/UnitTests.py
@@ -3,3 +3,17 @@ import CalculadoraImc
 
 def test_imc_abaixo_peso():
     assert round(CalculadoraImc.calcular_imc(50, 1.80), 2) < 18.5
+
+def test_imc_peso_normal():
+    assert 18.5 <= round(CalculadoraImc.calcular_imc(70, 1.75), 2) < 25
+
+def test_imc_sobrepeso():
+    assert 25 <= round(CalculadoraImc.calcular_imc(85, 1.75), 2) < 30
+
+def test_imc_obesidade():
+    assert round(CalculadoraImc.calcular_imc(95, 1.60), 2) >= 30
+
+def test_imc_zero_altura():
+    with pytest.raises(ZeroDivisionError):
+        CalculadoraImc.calcular_imc(70, 0)
+
