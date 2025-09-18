9d243658592aeaf893f7d8353267cbc8d133a793
Mateus Fonseca <71104330+mateusf-prog@users.noreply.github.com>
2025-09-18 20:21:44 -0300

    fix unit test

--- tests/UnitTests.py
+++ tests/UnitTests.py
@@ -2,4 +2,4 @@ import pytest
 import CalculadoraImc
 
 def test_imc_abaixo_peso():
-    assert round(calcular_imc(50, 1.80), 2) < 18.5
+    assert round(CalculadoraImc.calcular_imc(50, 1.80), 2) < 18.5
